7c3e9a21a46629a8cd31cce48d45f4061bf6d4fb
Pavithra Vemula <pavithravemula04@gmail.com>
2023-02-18 22:52:50 +0530

    Add project files.

---
+++ Controllers/InfoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ALR_02.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Info
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FAQs()
+        {
+            return View();
+        }
+        public ActionResult Reviews()
+        {
+            return View();
+        }
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+
+    }
+}
